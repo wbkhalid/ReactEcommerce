67c6d51368620c0f56433f94041ec8c3b067d882
wbkch <waleedbinkhalidch@gmail.com>
2022-10-26 21:06:50 +0500

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobleStyle } from './GlobleStyle';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import SingleProduct from './pages/SingleProduct';
 
-function App() {
+const App = () => {
+  const theme = {
+    colors: {
+      heading: 'rgb(24 24 29)',
+      text: 'rgba(29 ,29, 29, .8)',
+      white: '#fff',
+      black: ' #212529',
+      helper: '#8490ff',
+
+      bg: '#F6F8FA',
+      footer_bg: '#0a1435',
+      btn: 'rgb(98 84 243)',
+      border: 'rgba(98, 84, 243, 0.5)',
+      hr: '#ffffff',
+      gradient:
+        'linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)',
+      shadow:
+        'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;',
+      shadowSupport: ' rgba(0, 0, 0, 0.16) 0px 1px 4px',
+    },
+    media: {
+      mobile: '768px',
+      tab: '998px',
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobleStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
