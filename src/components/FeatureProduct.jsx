1a7c603848e149af04075cba6bdbc856a0db2328
wbkch <waleedbinkhalidch@gmail.com>
2022-10-30 20:22:55 +0500

    features component completed

---
+++ src/components/FeatureProduct.jsx
@@ -0,0 +1,135 @@
+import { useProductContext } from '../context/productcontext';
+import styled from 'styled-components';
+import Product from './Product';
+
+const FeatureProduct = () => {
+  const { isLoading, featureProducts } = useProductContext();
+
+  if (isLoading) {
+    return <div> ......Loading </div>;
+  }
+
+  return (
+    <Wrapper className="section">
+        <div className="common-heading">Our Feature Services</div>
+      <div className="container">
+        <div className="grid grid-three--column">
+          {featureProducts.map((curElem) => {
+            return <Product key={curElem.id} {...curElem} />;
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  padding: 1rem 0;
+  
+
+  .common-heading {
+      text-align: center;
+      text-transform: uppercase;
+    }
+
+  .container {
+    max-width: 125rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    padding: 5rem 5rem;
+    border-radius: 1rem;
+  }
+
+  figure {
+    width: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    transition: all 0.5s linear;
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 0%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      transition: all 0.2s linear;
+      cursor: pointer;
+    }
+    &:hover::after {
+      width: 100%;
+    }
+    &:hover img {
+      transform: scale(0.99);
+    }
+    img {
+      max-width: 90%;
+      margin-top: 1.5rem;
+      height: 20rem;
+      transition: all 0.2s linear;
+    }
+
+    .caption {
+      position: absolute;
+      top: 15%;
+      right: 10%;
+      text-transform: uppercase;
+      background-color: ${({ theme }) => theme.colors.bg};
+      color: ${({ theme }) => theme.colors.helper};
+      padding: 0.8rem 2rem;
+      font-size: 1.2rem;
+      border-radius: 2rem;
+      font-weight: 600;
+    }
+  }
+
+  .card {
+    background-color: #fff;
+    border-radius: 1rem;
+    padding: 1rem;
+
+    .card-data {
+      padding: 0 2rem;
+    }
+
+    .card-data-flex {
+      margin: 1rem 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    h3 {
+      color: ${({ theme }) => theme.colors.text};
+      text-transform: capitalize;
+    }
+
+    .card-data--price {
+      color: ${({ theme }) => theme.colors.helper};
+    }
+
+    .btn {
+      margin: 2rem auto;
+      background-color: rgb(0 0 0 / 0%);
+      border: 0.1rem solid rgb(98 84 243);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &:hover {
+        background-color: rgb(98 84 243);
+      }
+
+      &:hover a {
+        color: #fff;
+      }
+      a {
+        color: rgb(98 84 243);
+        font-size: 1.4rem;
+      }
+    }
+  }
+`;
+
+export default FeatureProduct;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import FeatureProduct from '../components/FeatureProduct';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Trusted from '../components/Trusted';
 
 const Home = () => {
+
   const data ={
     name:'WBK Store'
   }
   return (
     <>
+    
       <Hero mydata={data}/>
+      <FeatureProduct/>
       <Services/>
       <Trusted/>
     </>
